7e151d534228f74acb1c961faf94f1ff5955d645
ayyesu <iamdanielkissiedu@gmail.com>
2023-08-17 02:19:55 +0000

    Authentication UI setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Home from './components/Home';
 import SignUp from './components/SignUp';
 import SignIn from './components/Signin';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
@@ -8,6 +9,7 @@ function App() {
             <Routes>
                 <Route path='/' exact element={<SignUp />} />
                 <Route path='/signin' element={<SignIn />} />
+                <Route path='/home' element={<Home />} />
             </Routes>
         </Router>
     );
