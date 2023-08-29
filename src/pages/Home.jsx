d118c535f4dbfa48facfcbbdb51bfbdfebae3d1f
ayyesu <iamdanielkissiedu@gmail.com>
2023-08-29 15:14:51 +0000

    modified:   src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,98 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
 import NavBar from '../components/NavBar';
+import {AuthContext} from '../context/AuthContext';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import BookCard from '../components/BookCard';
 
-const Home = () => {
+function Copyright() {
     return (
-        <>
-            <NavBar />
-            <h1>Home</h1>
-        </>
+        <Typography variant='body2' color='text.secondary' align='center'>
+            {'Copyright © '}
+            <Link color='inherit' href='https://mui.com/'>
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
     );
-};
+}
 
-export default Home;
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function Album() {
+    const {user} = React.useContext(AuthContext);
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <CssBaseline />
+            <NavBar />
+            <main>
+                {/* Hero unit */}
+                <Box
+                    sx={{
+                        bgcolor: 'background.paper',
+                        pt: 8,
+                        pb: 6,
+                    }}
+                >
+                    <Container maxWidth='sm'>
+                        <Typography
+                            component='h1'
+                            variant='h2'
+                            align='center'
+                            color='text.primary'
+                            gutterBottom
+                        >
+                            {`Welcome ${user.user?.username}`}
+                        </Typography>
+                        <Typography
+                            variant='h5'
+                            align='center'
+                            color='text.secondary'
+                            paragraph
+                        >
+                            Something short and leading about the collection
+                            below—its contents, the creator, etc. Make it short
+                            and sweet, but not too short so folks don&apos;t
+                            simply skip over it entirely.
+                        </Typography>
+                    </Container>
+                </Box>
+                <Container sx={{py: 8}} maxWidth='md'>
+                    {/* End hero unit */}
+                    <Grid container spacing={4}>
+                        {cards.map((card) => (
+                            <Grid item key={card} xs={12} sm={6} md={4}>
+                                <BookCard />
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Container>
+            </main>
+            {/* Footer */}
+            <Box sx={{bgcolor: 'background.paper', p: 6}} component='footer'>
+                <Typography variant='h6' align='center' gutterBottom>
+                    Footer
+                </Typography>
+                <Typography
+                    variant='subtitle1'
+                    align='center'
+                    color='text.secondary'
+                    component='p'
+                >
+                    Something here to give the footer a purpose!
+                </Typography>
+                <Copyright />
+            </Box>
+            {/* End footer */}
+        </ThemeProvider>
+    );
+}
