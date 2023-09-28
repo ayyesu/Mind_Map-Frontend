f91dcfb1dc97999735b9d62a2836982cc4bbab25
ayyesu <iamdanielkissiedu@gmail.com>
2023-09-28 18:08:28 +0000

    configured firebase for cloud storage

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,33 @@ import {AuthContext} from './context/AuthContext';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import NotFound from './pages/NotFound';
+import Admin from './pages/Admin';
+import {BookContextProvider} from './context/BookContext';
 
 function App() {
     const {user} = useContext(AuthContext);
     return (
         <Router>
-            <ToastContainer />
-            <Routes>
-                <Route path='/' exact element={user ? <Home /> : <SignIn />} />
-                <Route path='/signup' element={user ? <Home /> : <SignUp />} />
-                <Route path='/signin' element={user ? <Home /> : <SignIn />} />
-                <Route path='*' element={<NotFound />} />
-            </Routes>
+            <BookContextProvider>
+                <ToastContainer />
+                <Routes>
+                    <Route
+                        path='/'
+                        exact
+                        element={user ? <Home /> : <SignIn />}
+                    />
+                    <Route
+                        path='/signup'
+                        element={user ? <Home /> : <SignUp />}
+                    />
+                    <Route
+                        path='/signin'
+                        element={user ? <Home /> : <SignIn />}
+                    />
+                    <Route path='/admin' element={<Admin />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+            </BookContextProvider>
         </Router>
     );
 }
