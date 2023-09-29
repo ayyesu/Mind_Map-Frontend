9f8daea2ba1d40fc175d1c13c7c473a9badbc723
ayyesu <iamdanielkissiedu@gmail.com>
2023-09-29 01:44:52 +0000

    modified book card

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,38 +1,46 @@
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import React from 'react';
 
-const BookCard = ({imageUrl}) => {
+const BookCard = ({imageUrl, title, description}) => {
+    const truncatedDescription = description.slice(0, 50) + '...';
+    const truncatedtitle = title.slice(0, 20) + '...';
     return (
         <Card
             sx={{
-                height: '100%',
+                width: '80%',
+                height: '95%',
                 display: 'flex',
                 flexDirection: 'column',
             }}
         >
-            <CardMedia
-                component='div'
-                sx={{height: 140}}
-                title='green iguana'
-                image={imageUrl}
-            />
-            <CardContent>
-                <Typography gutterBottom variant='h5' component='h2'>
-                    Heading
+            <CardMedia component='div' sx={{height: 240}}>
+                <img
+                    src={imageUrl}
+                    alt='Book Cover'
+                    title={title}
+                    style={{
+                        objectFit: 'cover',
+                        width: '100%',
+                        height: '100%',
+                    }}
+                />
+            </CardMedia>
+            <CardContent sx={{padding: '10px'}}>
+                <Typography
+                    gutterBottom
+                    variant='h6'
+                    component='h4'
+                    fontSize='1.0rem'
+                >
+                    {truncatedtitle}
                 </Typography>
-                <Typography>
-                    This is a media card. You can use this section to describe
-                    the content.
+                <Typography fontSize='0.8rem'>
+                    {truncatedDescription}
                 </Typography>
             </CardContent>
-            <CardActions>
-                <Button size='small'>View</Button>
-            </CardActions>
         </Card>
     );
 };
