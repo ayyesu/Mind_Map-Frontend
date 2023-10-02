00c29b8258aab608e28acc82e11d1c465c1c5264
ayyesu <iamdanielkissiedu@gmail.com>
2023-10-02 23:28:14 +0000

    setup protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,31 @@ import SignUp from './components/SignUp';
 import SignIn from './components/Signin';
 import Home from './pages/Home';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import {useContext} from 'react';
-import {AuthContext} from './context/AuthContext';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import NotFound from './pages/NotFound';
 import Admin from './pages/Admin';
-import {BookContextProvider} from './context/BookContext';
 import {register} from './serviceWorker';
+import BookDetailsPage from './pages/BookDetails';
+import ProtectedRoute from './ProtectedRoutes';
 
 function App() {
     register();
-    const {user} = useContext(AuthContext);
+
     return (
         <Router>
-            <BookContextProvider>
-                <ToastContainer />
-                <Routes>
-                    <Route
-                        path='/'
-                        exact
-                        element={user ? <Home /> : <SignIn />}
-                    />
-                    <Route
-                        path='/signup'
-                        element={user ? <Home /> : <SignUp />}
-                    />
-                    <Route
-                        path='/signin'
-                        element={user ? <Home /> : <SignIn />}
-                    />
+            <ToastContainer />
+            <Routes>
+                <Route element={<ProtectedRoute />}>
+                    <Route path='/' element={<Home />} />
                     <Route path='/admin' element={<Admin />} />
-                    <Route path='*' element={<NotFound />} />
-                </Routes>
-            </BookContextProvider>
+                    <Route path='/book/:bookId' element={<BookDetailsPage />} />
+                </Route>
+                <Route path='/signin' exact element={<SignIn />} />
+                <Route path='/signup' element={<SignUp />} />
+
+                <Route path='*' element={<NotFound />} />
+            </Routes>
         </Router>
     );
 }
