f9449c09d2802f3b4a4f33fc46126505d24ec27b
ayyesu <iamdanielkissiedu@gmail.com>
2023-11-02 00:15:42 +0000

    api url integrated

--- src/utils/Service.jsx
+++ src/utils/Service.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseUrl = "http://localhost:5000";
+export const baseUrl = "https://mind-map-api.onrender.com";
 
 // GET request
 export const getRequest = async (url) => {
