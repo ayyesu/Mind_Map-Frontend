7b0c82f34cd06c124115ac88f186cea6b4915ded
ayyesu <iamdanielkissiedu@gmail.com>
2023-11-14 20:57:58 +0000

    updated pdf-dist package

--- src/components/PDFViewer.jsx
+++ src/components/PDFViewer.jsx
@@ -6,7 +6,7 @@ import "@react-pdf-viewer/core/lib/styles/index.css";
 const PDFViewer = ({ pdfFile }) => {
   return (
     <div>
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
         <Viewer fileUrl={pdfFile} />
       </Worker>
     </div>
