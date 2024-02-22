7556243809faf4391dd247c3beb14ca12681200c
ayyesu <iamdanielkissiedu@gmail.com>
2024-02-22 00:07:38 +0000

    modified BookCard to fit theme

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -2,21 +2,20 @@ import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import React from 'react';
+import React, {useContext} from 'react';
+import {FunctionContext} from '../context/functionContext';
 
 const BookCard = ({imageUrl, title, description}) => {
     const truncatedDescription = description.slice(0, 50) + '...';
     const truncatedtitle = title.slice(0, 20) + '...';
 
+    const {currentTheme} = useContext(FunctionContext);
+
     return (
         <Card
-            sx={{
-                width: '80%',
-                height: '95%',
-                display: 'flex',
-                flexDirection: 'column',
-                borderRadius: '10px',
-            }}
+            className={`w-80 h-95 flex flex-col rounded-2xl ${
+                currentTheme === 'dark' ? 'bg-slate-800 text-white' : ''
+            }`}
         >
             <CardMedia component='div' sx={{height: 240}}>
                 <img
@@ -32,7 +31,11 @@ const BookCard = ({imageUrl, title, description}) => {
                     }}
                 />
             </CardMedia>
-            <CardContent sx={{padding: '10px'}}>
+            <CardContent
+                className={`${
+                    currentTheme === 'dark' ? 'bg-slate-800' : ''
+                } p-10`}
+            >
                 <Typography
                     gutterBottom
                     variant='h6'
