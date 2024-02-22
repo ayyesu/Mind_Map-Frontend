17a7d3645b5bd976c30f8842f5708ab24dec35f2
ayyesu <iamdanielkissiedu@gmail.com>
2024-02-22 00:04:51 +0000

    functional control context

--- src/context/functionContext.jsx
+++ src/context/functionContext.jsx
@@ -5,9 +5,12 @@ import {toast} from 'react-toastify';
 export const FunctionContext = createContext();
 
 export const FunctionContextProvider = ({children}) => {
+    // Waitlist
     const [joiningWaitlist, setJoininghWaitlist] = useState(false);
     const [waitlistError, setWaitlistError] = useState('');
     const [waitlistJoined, setWaitlistJoined] = useState(false);
+
+    const [currentTheme, setCurrentTheme] = useState(localStorage.theme);
 
     // Joining Waitlist
     const joinWaitlist = async (email) => {
@@ -36,6 +39,25 @@ export const FunctionContextProvider = ({children}) => {
         }
     };
 
+    // Theme functionality
+    const handleThemeChange = () => {
+        if (
+            localStorage.theme === 'dark' ||
+            (!('theme' in localStorage) &&
+                window.matchMedia('(prefers-color-scheme: dark)').matches)
+        ) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    };
+
+    const setTheme = (theme) => {
+        localStorage.theme = theme;
+        setCurrentTheme(localStorage.theme);
+        handleThemeChange();
+    };
+
     return (
         <FunctionContext.Provider
             value={{
@@ -43,6 +65,9 @@ export const FunctionContextProvider = ({children}) => {
                 joiningWaitlist,
                 waitlistError,
                 waitlistJoined,
+                handleThemeChange,
+                setTheme,
+                currentTheme,
             }}
         >
             {children}
